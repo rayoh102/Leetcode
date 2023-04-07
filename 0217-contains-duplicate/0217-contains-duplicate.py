94a49012c26ded6038c2e4a844eac3b141591cbc
Ray Oh <75292027+rayoh102@users.noreply.github.com>
2023-04-07 01:42:00 -0700

    Time: 447 ms (66.74%), Space: 28.5 MB (27.23%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        hashSet = set()
+        
+        for n in nums: 
+            if n in hashSet:
+                return True
+            hashSet.add(n)
+        return False 
