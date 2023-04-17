8ec529e1650815a57dd5db654b905c3fac7d2ea7
Ray Oh <75292027+rayoh102@users.noreply.github.com>
2023-04-16 22:32:05 -0700

    Time: 20 ms (83.10%), Space: 13.4 MB (87.19%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,21 +10,29 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        temp = ListNode()
-        tail = temp
+        # Create a dummy node inside our list to avoid adding to an empty list
+        dummy = ListNode()
+        # Tail of our list = our dummy node
+        tail = dummy
 
         while list1 and list2:
+            # if list1's value < list2's value, add list1's value to our list and iterate to next value of list1
             if list1.val < list2.val:
                 tail.next = list1
                 list1 = list1.next
+
+            # else list1's value > list2's value, add list2's value to our list and iterate to next value of list2
             else:
                 tail.next = list2
                 list2 = list2.next
+            
+            # Update the tail of our listnode
             tail = tail.next
 
+        # If list1 isn't empty yet, then add the remaining values of list1 and add it to our list
         if list1:
             tail.next = list1
         elif list2:
             tail.next = list2
 
-        return temp.next
+        return dummy.next
